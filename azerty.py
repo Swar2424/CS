41711796fd2ca53f4fb30af78043b17570f33344
Swar2424 <154347168+Swar2424@users.noreply.github.com>
2024-10-25 11:49:01 +0900

    tests

--- azerty.py
+++ azerty.py
@@ -1,4 +1,5 @@
 #%%
+import scipy.io as sio
 import numpy as np
 from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
@@ -6,44 +7,26 @@ from matplotlib import pyplot as plt
 from sklearn.svm import LinearSVC
 from sklearn.metrics import confusion_matrix
 
-X, y = make_blobs(n_samples=250, centers=2,
-                  random_state=0, cluster_std=0.60)
-y[y == 0] = -1 # labels as -1 or +1
-tmp = np.ones(len(X))
-y = tmp * y
 
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap='winter')
+mat_content = sio.loadmat('face.mat')
+face_data = mat_content['X']
+N = len(face_data)
+M = 100
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+mean = np.mean(np.stack(face_data, axis=1), axis=0)
 
+#print(mean, mean.shape)
 
-our_svm = LinearSVC()
-our_svm.fit(X_train, y_train) # training function
+#print(face_data[:,0], face_data[:,0].shape)
 
-plt.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap='winter');
-ax = plt.gca()
-xlim = ax.get_xlim()
-w = our_svm.coef_[0]
-a = -w[0] / w[1]
-xx = np.linspace(xlim[0], xlim[1])
+A = (face_data.T - mean)
 
-yy = a * xx - our_svm.intercept_[0] / w[1]
-plt.plot(xx, yy)
+S = 1/N * (np.dot(A,A.T))
 
-yy = a * xx - (our_svm.intercept_[0] - 1) / w[1]
-plt.plot(xx, yy, 'k--')
+eigvals, eigvecs = np.linalg.eig(S)
 
-yy = a * xx - (our_svm.intercept_[0] + 1) / w[1]
-plt.plot(xx, yy, 'k--')
+s_eigvecs = [x for _,x in sorted(zip(eigvals,eigvecs))][M:]
+s_eigvals = sorted(eigvals)[M:]
 
-y_pred = our_svm.predict(X_test)
-cm=confusion_matrix(y_test, y_pred)
-
-plt.matshow(cm, cmap = 'Blues')
-plt.colorbar()
-plt.ylabel('Actual')
-plt.xlabel('Predicted')
-plt.show()
-
-plt.show()
-
+print(s_eigvals)
+#print(s_eigvecs)
